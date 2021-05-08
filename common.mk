045b583b7126126fea939dbb6bb6547456c5d6de
135e2 <51870710+135e2@users.noreply.github.com>
2021-05-08 19:05:44 +0800

    msm8998-common: Switch to los kernel

--- common.mk
+++ common.mk
@@ -538,9 +538,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# NezukoOS
-NEZUKO_BUILD_TYPE := OFFICIAL
-NEZUKO_MAINTAINER := Maitreya29
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
